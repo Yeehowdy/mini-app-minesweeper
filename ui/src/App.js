a49316f0b638544c4dc8ee40c12154d8a09e208c
Yeehowdy <kylemersinger67@gmail.com>
2023-04-04 21:38:02 +0200

    Didnt finish :(

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,9 +1,25 @@
 import Board from "./Board";
+import { useState, useContext, createContext } from "react";
+
 
 function App() {
+  const [gameConfig, setGameConfig] = useState({rows: 10, cols: 10, mines: 30})
+  const [isPlaying, setIsPlaying] = useState(false)
+
+  const handleChange = (type, value) => {
+    let changedGameCongig = {...gameConfig,}
+    changedGameCongig[type] = value;
+    setGameConfig(changedGameCongig)
+  }
+
   return (
     <div className="App">
-      <Board/>
+      <input className="w-25" type="number" min={3} max={30} placeholder="# of Rows" onChange={(e) => handleChange("rows", e.target.value)} disabled={isPlaying}/>
+      <input className="w-25" type="number" min={3} max={30} placeholder="# of Cols" onChange={(e) => handleChange("cols", e.target.value)} disabled={isPlaying}/>
+      <input className="w-25" type="number" min={3} max={(gameConfig.rows * gameConfig.cols) - 1} placeholder="# of Mines" onChange={(e) => handleChange("mines", e.target.value)} disabled={isPlaying}/>
+      <button className="w-25" onClick={() => setIsPlaying(true)} disabled={isPlaying}>Start Game</button>
+      {isPlaying ? (<Board config={gameConfig}/>) : ''}
+      
     </div>
   );
 }
--- ui/src/Board.js
+++ ui/src/Board.js
@@ -2,51 +2,63 @@ import { useState, useEffect } from "react";
 import { Row, Col, Container, Modal } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function Board() {
-  const [board, setBoard] = useState([
-    [1, 2, 3],
-    [4, 5 ,6],
-  ]);
-
+function Board(props) {
+  const [board, setBoard] = useState([]);
   const [gameOver, setGameOver] = useState(false)
   const [safeCells, setSafeCells] = useState(1)
   const [bombCells, setBombCells] = useState(1)
+  // const [runningTime, setRunningTime] = useState()
+  // const [startTime, setStartTime] = useState()
 
   useEffect(() => {
     let newBoard = [];
     let bombCount = 0;
-    setGameOver(false)
-    for(let x=0; x < 10; x++){
+    for(let x=0; x < props.config.rows; x++){
       let newRow = []
-      for(let y=0;y<10;y++){
-        let cell = {isBomb: false, isClicked: false, adjacentCells: 0}
-        let bombRoll = Math.round(Math.random(1) * 10)
-        if(bombRoll > 7 && bombCount < 30){
-          cell.isBomb = true
-          bombCount++
-        }
+      for(let y=0;y<props.config.cols;y++){
+        let cell = {isBomb: false, isClicked: false, flaggable: true}
+        // let bombRoll = Math.round(Math.random(1) * 10)
+        // if(bombRoll > 7 && bombCount < 30){
+        //   cell.isBomb = true
+        //   bombCount++
+        // }
         newRow.push(cell)
       }
       newBoard.push(newRow)
     }
+    // setInterval(() => {setRunningTime(Date.now() - startTime)}, 200)
     setBombCells(bombCount)
     setSafeCells(100-bombCount)
+    // setStartTime(Date.now())
+    // setRunningTime(null)
     setBoard(newBoard)
   }, [gameOver])
+
+  const makeMines = (mines) => {
+    let minesLeft = mines
+    let rows = board.length - 1
+    let cols = board[0].length - 1
+
+  }
 
   const flagCell = (row, col) => {
     let mutatedBoard = [...board]
     let cell = mutatedBoard[row][col]
-    if(cell.adjacentCells == 'F'){
-      cell.adjacentCells = null
-      setBombCells(bombCells +1)
-    }
-    else{
-      cell.adjacentCells = 'F'
-      setBombCells(bombCells -1)
+    if(cell.flaggable){
+      if(cell.adjacentCells == 'F'){
+        cell.isClicked = false
+        cell.adjacentCells = null
+        setBombCells(bombCells +1)
+      }
+      else{
+        cell.isClicked = true
+        cell.adjacentCells = 'F'
+        setBombCells(bombCells -1)
+      }
+      
+      setBoard(mutatedBoard)
     }
     
-    setBoard(mutatedBoard)
     
   }
 
@@ -54,6 +66,7 @@ function Board() {
     let mutatedBoard = [...board]
     let cell = mutatedBoard[row][col]
     cell.isClicked = true;
+    cell.flaggable = false;
     if(cell.isBomb){
       setGameOver(true)
       console.log('BOOM! Game over')
@@ -78,16 +91,19 @@ function Board() {
     setBoard(mutatedBoard)
   }
 
+  
+
   return (
     <Container className="w-75">
       <h3 className="text-center">{safeCells} safe cells remain</h3>
       <h3 className="text-center">{bombCells} bombs remain</h3>
+      {/* <h3 className="text-center">{runningTime}</h3> */}
       {board.map((row, rowIndex) => 
         (
         <Row key={rowIndex}>
           {row.map((cell, colIndex) => 
             (<Col className="border" key={[rowIndex, colIndex]} >
-              <button className="w-100 h-100" onClick={() => cellCheck(rowIndex, colIndex)} onContextMenu={() => flagCell(rowIndex, colIndex)}>
+              <button className="w-100 h-100" onClick={() => cellCheck(rowIndex, colIndex)} onContextMenu={(e) => {e.preventDefault();flagCell(rowIndex, colIndex)}} disabled={cell.isClicked}>
                 {cell.isClicked || cell.adjacentCells ? cell.adjacentCells : '' }
               </button>
             </Col>)
